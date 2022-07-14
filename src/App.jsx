e4ec410fd79c162b2409b7ca28e8fffc1caa8277
JAZ-POOJA <72659717+JAZ-POOJA@users.noreply.github.com>
2022-07-14 16:45:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Product from "./pages/Product";
+import Home from "./pages/home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+//import Cart from "./pages/Cart";
+
+const App = () => {
+  return <Register/>;
+};
+
+export default App;
